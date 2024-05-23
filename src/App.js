9e4c57944856d1369968d2f5cfad60d6744cf059
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-05-23 23:04:55 +0530

    update: update in code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Index from "./components/Index";
 import Footer from "./components/Footer";
 import MoMForm from "./components/MoMForm";
+import First from "./components/First";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />}></Route>
+        <Route path="/" element={<First />}></Route>
+        <Route path="/Index" element={<Index />}></Route>
         <Route path="/Header" element={<Header />}></Route>
         <Route path="/Footer" element={<Footer />}></Route>
         <Route path="/MoMForm" element={<MoMForm />}></Route>
---
+++ src/components/First.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unreachable */
+const First = () => {
+  return (
+    <>
+      <div className="h-[100vh] w-[100vw]"></div>
+    </>
+  );
+};
+export default First;
