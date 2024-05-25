c1200df201597e309afb0eaed511a8fb6c7267e5
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-05-25 23:17:40 +0530

    update: update in web development task

--- src/components/Hiringdoc.jsx
+++ src/components/Hiringdoc.jsx
@@ -1,5 +1,5 @@
 import jsPDF from "jspdf";
-
+import moment from "moment";
 const HiringDoc = () => {
   const getPDF = (e) => {
     e.preventDefault();
@@ -11,7 +11,7 @@ const HiringDoc = () => {
     doc.setFontSize(12);
     doc.setLineWidth(1);
 
-    doc.line(20, 22, 160, 22);
+    doc.line(20, 22, 190, 22);
     doc.text(`Dear uname,`, 20, 29);
 
     doc.setFont("serif", "normal");
@@ -48,17 +48,51 @@ const HiringDoc = () => {
     doc.text(`5. Joining Date: 25th June 2024`, 20, 111);
     doc.text(`6. Timing : Flexible`, 20, 121);
     doc.text(`7. Hours Neeed : 18 hours/week(5 days Week)`, 20, 131);
-    doc.setFont("serif", "bold");
-    doc.text(`The Responsibilites are:`, 20, 141);
+    doc.text(`8. Duration : 6 months`, 20, 141);
     doc.setFont("serif", "normal");
-    doc.text(`1. Managing day to day web development updates`, 20, 151);
-    doc.text(`2. Communicating with team as per project requirement`, 20, 161);
-    doc.text(`3. Working for 18 hours/week`, 20, 171);
     doc.text(
-      `4. Working primarily on frontend devlopment and API integration`,
+      `The main roles & responsibilites for the web development intern role are:`,
+      20,
+      151
+    );
+    doc.setFont("serif", "normal");
+    doc.text(
+      `1. Managing day to day web development updates as per needs of the project or feature`,
+      20,
+      161
+    );
+    doc.text(`2. Communicating with team as per project requirement`, 20, 171);
+    doc.text(
+      `3. Developing robust APIs and integrating its data in the frontend as per the need`,
       20,
       181
     );
+    doc.text(
+      `4. Working primarily on react.js for frontend devlopment and express.js,node.js and MongoDB for backend`,
+      20,
+      191
+    );
+
+    doc.text(
+      `This internship will not only help you to boost your skills but also have a multiple benefits which are:`,
+      20,
+      201
+    );
+    doc.text(`1. Letter of recommendation`, 20, 211);
+    doc.text(`2. Internship Certificate `, 20, 221);
+    doc.text(`3. Flexible work Hours`, 20, 231);
+    doc.text(
+      `4. For full-time jobs priority will be given to top performing interns `,
+      20,
+      241
+    );
+    doc.setFont("serif", "normal");
+    doc.setFontSize(12);
+    doc.text(`Aritra Chakraborty`, 20, 270);
+    doc.text(`( Technical Head )`, 20, 277);
+    doc.setFont("serif", "normal");
+    doc.setFontSize(10);
+    doc.text("Date: " + moment().format("Do MMM YYYY"), 21, 285);
 
     doc.save("unameHiringLetter.pdf");
   };
